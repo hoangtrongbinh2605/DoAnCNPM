9cc361c3e8276eec9ebe548693f9d6955989a7e8
tamletan <tamletannk94@gmail.com>
2019-10-05 15:38:37 +0700

    login-signup

---
+++ Esport/app/src/main/java/com/example/esport/LoginActivity.java
@@ -0,0 +1,4 @@
+package com.example.esport;
+
+public class LoginActivity {
+}
--- Esport/app/src/main/java/com/example/esport/MainActivity.java
+++ Esport/app/src/main/java/com/example/esport/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.esport;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.NavUtils;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,18 +22,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
-
+    DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -43,8 +46,22 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
+//        findViewById(R.id.lin).setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                setContentView(R.layout.activity_login);
+//            }
+//        });
     }
-
+    public void signup_action(View v) {
+        setContentView(R.layout.activity_signup);
+    }
+    public void login_action(View v) {
+        setContentView(R.layout.activity_login);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -58,4 +75,32 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.mn_login:
+//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                setContentView(R.layout.activity_login);
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+//    private long backPressedTime;
+//    private Toast backToast;
+//    @Override
+//    public void onBackPressed() {
+//        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+//            backToast.cancel();
+////            super.onBackPressed();
+//            NavUtils.navigateUpFromSameTask(this);
+//            return;
+//        } else {
+//            backToast = Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT);
+//            backToast.show();
+//        }
+//
+//        backPressedTime = System.currentTimeMillis();
+//    }
 }
